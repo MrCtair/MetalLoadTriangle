de77656030bd100f54d446a71f1c6ee066f10d10
ctair.china <33180314@qq.com>
2020-08-20 18:05:45 +0800

    三角形加载也添加

---
+++ MetalSimpleExample/MetalSimpleExample/LoadTriangle/LoadTriangleViewController.h
@@ -0,0 +1,17 @@
+//
+//  LoadTriangleViewController.h
+//  MetalSimpleExample
+//
+//  Created by trs on 2020/8/20.
+//  Copyright © 2020 ctair. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoadTriangleViewController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MetalSimpleExample/MetalSimpleExample/LoadTriangle/LoadTriangleViewController.m
@@ -0,0 +1,32 @@
+//
+//  LoadTriangleViewController.m
+//  MetalSimpleExample
+//
+//  Created by trs on 2020/8/20.
+//  Copyright © 2020 ctair. All rights reserved.
+//
+
+#import "LoadTriangleViewController.h"
+
+@interface LoadTriangleViewController ()
+
+@end
+
+@implementation LoadTriangleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
